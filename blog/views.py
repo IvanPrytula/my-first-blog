dfa187cf12208cc3def3907482a0c296fec02c77
IvanPrytula <johnnyforester@gmail.com>
2018-10-28 23:29:04 +0200

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def hello(request):
-    return HttpResponse('hello')
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
